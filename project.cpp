b248c0633416f7c31693bf3cb3389eb83558b373
theo <theotufford11@gmail.com>
2026-02-11 22:12:12 -0600

    forked into oop version
    dont know if this is a good idea to overwhelm the gang with oop so early
    but I think they can handle it, forked just in case.

--- project.cpp
+++ project.cpp
@@ -6,28 +6,58 @@
 
 using namespace std;
 
-// Setting up board to have 6 rows and 5 columns for the tries(6) and word
-// length(5)
-const int guess_max = 6;
-const int secret_length = 5;
+/* ANSI escape codes
+   Tells the terminal to change the background to a differet color using ANSI
+   Escape Codes
+   \033 is command and tells it what comes next is a special character, [
+   starts the sequence numbers like 42 tell it to choose green background
+   while the m ends the command sequence Always end with reset ANSI code to
+   reset the color back to normal
+*/
+class Board {
+public:
+  const int guess_max = 6;
+  const int secret_length = 5;
+  // ANSI color escape codes associated with their meaning
+  const string white = "\033[47m";     // 47 - white background
+  const string correct = "\033[42m";   // 42 - green background
+  const string contained = "\033[43m"; // 43 - yellow background
+  const string wrong = "\033[100m";    // 100 -bright black background
+  const string dark_text = "\033[30m"; // 30 - black text (in the foreground)
+  const string reset_code = "\033[0m"; // 0 - reset (styles and colors)
 
-// Tells the terminal to change the background to a differet color using ANSI
-// Escape Codes
-// \033 is command and tells it what comes next is a special character, [ starts
-// the sequence numbers like 42 tell it to choose green background while the m
-// ends the command sequence Always end with reset ANSI code to reset the color
-// back to normal
+  vector<string> guess_history;
+  string secret;
+  void printBoard();
+  vector<string> get_guess_colors(string guess);
+  void board_test();
+};
 
-const string white = "\033[47m";          // 47 - white background
-const string correct = "\033[42m";          // 42 - green background
-const string contained = "\033[43m";         // 43 - yellow background
-const string wrong = "\033[100m";          // 100 -bright black background
-const string dark_text_code = "\033[30m"; // 30 - black text (in the foreground)
-const string reset_code = "\033[0m";      // 0 - reset (styles and colors)
+vector<string> Board::get_guess_colors(string guess) {
+  vector<string> color_map;
+  /*
+  input: guess string
+
+  return:
+  vector of strings that encode the color of the tile at the
+  same index in the guess string
+
+  the colors are encoded as strings, hence the output vector being a vector of
+  strings
+
+  example:
+  secret string:  apple
+  guess: plume
+
+  output:
+  {contained, contained, wrong, wrong, correct}
+
+  */
+  return color_map;
+}
 
 // Print the board
-void printBoard(vector<string> guess_history,
-                vector<vector<string>> color_coding) {
+void Board::printBoard() {
   cout << "\n";
   // iterate through every tile on board
   for (int guess_hist_ind = 0; guess_hist_ind < guess_max; guess_hist_ind++) {
@@ -37,7 +67,7 @@ void printBoard(vector<string> guess_history,
 
       // get string in guess history and its corresponding color coding
       string guess_string = guess_history.at(guess_hist_ind);
-      vector<string> guess_color_map = color_coding.at(guess_hist_ind);
+      vector<string> guess_color_map = get_guess_colors(guess_string);
 
       // iterate through guess string
       for (int guess_ind = 0; guess_ind < secret_length; guess_ind++) {
@@ -46,10 +76,9 @@ void printBoard(vector<string> guess_history,
         char letter = guess_string.at(guess_ind);
         string ansi_color_code = guess_color_map.at(guess_ind);
 
-
         // print letter to the screen
-        cout << ansi_color_code << dark_text_code << " "
-             << (char)toupper(letter) << " " << reset_code << " ";
+        cout << ansi_color_code << dark_text << " " << (char)toupper(letter)
+             << " " << reset_code << " ";
       }
 
     } else {
@@ -62,31 +91,23 @@ void printBoard(vector<string> guess_history,
   }
 }
 
-// test to see if board works
-int board_test() {
-  vector<string> guess_history_test = {"crane", "slate"};
-  vector<vector<string>> color_coding_test = {
-      {contained, correct, correct, wrong, correct}, {contained, wrong, wrong, wrong, correct}};
-  printBoard(guess_history_test, color_coding_test);
-  return 0;
-}
-
-int main() {
+// test board rendering
+void Board::board_test() {
   cout << "testing board rendering: " << endl;
-  board_test();
+  secret = "plane";
+  guess_history = {"crane", "slate"};
+  printBoard();
+
+  // blocking stall until enter from user
   int tmp;
   cin >> tmp;
   system("clear");
+}
 
-  // variable setup
-  vector<string> player1_guess_history;
-  vector<string> player2_guess_history;
+int main() {
+  Board(player1_board);
+  Board(player2_board);
 
-  string player1_secret = "";
-  string player2_secret = "";
+  Board active_board = player1_board;
 
-  int player1_score = 0;
-  int player2_score = 0;
-
-  // main loop
 }
